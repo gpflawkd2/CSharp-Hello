b132ea586c88ad4a992e6a5cc98f141b7ca545a9
HyeRim Park <gpflawkd2@gmail.com>
2021-02-25 17:53:04 +0900

    Add while loop

--- ConsoleApplication/Program.cs
+++ ConsoleApplication/Program.cs
@@ -10,55 +10,54 @@ namespace ConsoleApplication
     {
         static void Main(string[] args)
         {
-
-            //C# 변수타입 -> 소문자로 시작
-            /*
-            short shortNum = 0;
-            int intNum = 0;
-            double doubleNum = 0;
-            float floatNum = 0;     //부동소수점 자리
-            string stringText = "";
-            char charText = 'a';
-            bool boolVal = true;
-            */
-
-            //.Net Framework 변수타입
-            /*
-            Int16 dotNetShort = 0;
-            Int32 dotNetInt = 0;
-            Int64 dotNetDouble = 0;
-            String dotNetString = "";
-            */
-
-            //var dynamicType = false;
-
             //** if문 **
-            var num = 1;
+            //var num = 1;
 
-            Console.WriteLine("0 ~ 9 사이의 값을 입력 : ");
+            //Console.WriteLine("0 ~ 9 사이의 값을 입력 : ");
 
-            //입력값
-            var input = Console.ReadLine();
-
-            //ToString() : String 으로 변환
+            //var input = Console.ReadLine();
 
             //if (num.ToString() == input)
             //{
             //    Console.WriteLine("같은 값을 입력했습니다!");
             //}
-            //else
+            //else if (num.ToString() != input)
             //{
             //    Console.WriteLine("다른 값을 입력했습니다!");
             //}
 
-            if (num.ToString() == input)
+
+            //** While문 **
+            var num = 1;
+
+            //while(num < 10)
+            //{
+            //    Console.WriteLine(++num);   //전위연산자
+            //}
+
+            //무한Loop
+            //while (true)
+            //{
+            //    num++;
+
+            //    if (num % 3 == 0)
+            //    {
+            //        continue;   //하단 while문 코드를 통과함
+            //    }
+
+            //    Console.WriteLine(num);
+
+            //    if (num == 100)
+            //    {
+            //        break;  //while문을 빠져나옴
+            //    }
+            //}
+
+            //한번은 실행됨
+            do
             {
-                Console.WriteLine("같은 값을 입력했습니다!");
-            }
-            else if (num.ToString() != input)
-            {
-                Console.WriteLine("다른 값을 입력했습니다!");
-            }
+                Console.WriteLine(num++);
+            } while (num < 10);
 
         }
     }
