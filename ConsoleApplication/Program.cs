b88ebb9724f932c058857d008f616d90b9415b7d
HyeRim Park <gpflawkd2@gmail.com>
2021-02-26 11:36:42 +0900

    Add for loop

--- ConsoleApplication/Program.cs
+++ ConsoleApplication/Program.cs
@@ -10,55 +10,39 @@ namespace ConsoleApplication
     {
         static void Main(string[] args)
         {
-            //** if문 **
+            //** for문 **
+
+            //for (var i = 0; i<=10; i++)
+            //{
+            //    Console.WriteLine(i);
+            //}
+
             //var num = 1;
 
-            //Console.WriteLine("0 ~ 9 사이의 값을 입력 : ");
-
-            //var input = Console.ReadLine();
-
-            //if (num.ToString() == input)
+            //for (; num <= 10; num++)
             //{
-            //    Console.WriteLine("같은 값을 입력했습니다!");
-            //}
-            //else if (num.ToString() != input)
-            //{
-            //    Console.WriteLine("다른 값을 입력했습니다!");
-            //}
-
-
-            //** While문 **
-            var num = 1;
-
-            //while(num < 10)
-            //{
-            //    Console.WriteLine(++num);   //전위연산자
-            //}
-
-            //무한Loop
-            //while (true)
-            //{
-            //    num++;
-
-            //    if (num % 3 == 0)
-            //    {
-            //        continue;   //하단 while문 코드를 통과함
-            //    }
-
             //    Console.WriteLine(num);
-
-            //    if (num == 100)
-            //    {
-            //        break;  //while문을 빠져나옴
-            //    }
             //}
 
-            //한번은 실행됨
-            do
-            {
-                Console.WriteLine(num++);
-            } while (num < 10);
+            //Array List
+            List<int> numberList = new List<int>();
 
+            numberList.Add(1);
+            numberList.Add(2);
+            numberList.Add(3);
+            numberList.Add(4);
+            numberList.Add(5);
+
+            //foreach : 특정 object에 있는 값을 모두 출력함
+            foreach (var num in numberList)
+            {
+                Console.WriteLine(num);
+            }
+
+            for (var index = 0; index < numberList.Count; index++)
+            {
+                Console.WriteLine(numberList[index]);
+            }
         }
     }
 }
