a906bf5a854418251c4bc7732e5e04fab5aa6afc
gpflawkd2 <gpflawkd2@gmail.com>
2021-03-25 15:59:28 +0900

    Add Class Library Discription Region

--- ConsoleApplication/Program.cs
+++ ConsoleApplication/Program.cs
@@ -15,11 +15,15 @@ namespace ConsoleApplication
             //calc.PrintHello();
             //Console.WriteLine(calc.Plus(10, 20));
 
+            #region + About Class Library
+
             //Console 프로그램에서 Main 함수는 하나여야 하므로 Project 추가시 Class Library(DLL 파일을 만들 때 사용) 선택
 
             // 다른 projcet의 Class 사용시 Reference에 등록 후 사용
             // 1)Reference 우클릭 > Add Reference > Projects > Solution > 체크
             // 2)Ctrl + .(점): 인텔리센스를 사용하여 using~ 구문 추가
+
+            #endregion
 
             FirstClass first = new FirstClass();
             first.FirstClassPrint();
